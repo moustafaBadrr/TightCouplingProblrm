b3e874dad050279bc0a537d61eaef230792d4277
saber <saber@DESKTOP-E7CHFII>
2025-07-25 15:34:57 +0200

    saber
    Update weather summaries in WeatherForecastController
    
    Removed "Scorching" and added "saber" to the Summaries array.

--- GitPractice/Controllers/WeatherForecastController.cs
+++ GitPractice/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GitPractice.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering","saber"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
